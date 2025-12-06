202f32fe77f839f0a914977d7eab4d9597062910
harshitnayancer-byte <harshitnayancer@gmail.com>
2025-12-06 12:37:15 +0000

    feature: added fonts and themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,15 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class", //we will toggle a class
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: "rgb(var(--bg) / <alpha-value>)",
+        fg: "rgb(var(--fg) / <alpha-value>)",
+        accent: "rgb(var(--accent) / <alpha-value)",
+      },
+    },
   },
   plugins: [],
 }
